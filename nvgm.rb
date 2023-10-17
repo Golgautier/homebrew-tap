8663ba968715446d7a31052866cf8b104d556454
Gautier <gautier.leblanc@gmail.com>
2023-10-17 18:02:34 +0200

    Brew formula update for nutanix_vg_manager version v0.1.3

--- nvgm.rb
+++ nvgm.rb
@@ -5,11 +5,11 @@
 class Nvgm < Formula
   desc ""
   homepage "https://github.com/Golgautier/nutanix_vg_manager"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
-    url "https://github.com/Golgautier/nutanix_vg_manager/releases/download/v0.1.2/nutanix_vg_manager_0.1.2_darwin_all.tar.gz"
-    sha256 "ac9577ca8153bf764286989eec6fa1e689f19a3e30993d75820eb98920195c80"
+    url "https://github.com/Golgautier/nutanix_vg_manager/releases/download/v0.1.3/nutanix_vg_manager_0.1.3_darwin_all.tar.gz"
+    sha256 "07e0b105b3dc77b1bdb562c5ace932f3b29119074dccf407f6c28713cc8f65db"
 
     def install
       bin.install "nvgm"
@@ -18,16 +18,16 @@ class Nvgm < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Golgautier/nutanix_vg_manager/releases/download/v0.1.2/nutanix_vg_manager_0.1.2_linux_arm64.tar.gz"
-      sha256 "a02e96e9522edabef8e132b657d032e782d0dced98fece0ff686a7507f0a473c"
+      url "https://github.com/Golgautier/nutanix_vg_manager/releases/download/v0.1.3/nutanix_vg_manager_0.1.3_linux_arm64.tar.gz"
+      sha256 "664889f69ac01dfa7d23aeafc04cbea5d85f2c4bbe2cd304f7773269996cd37c"
 
       def install
         bin.install "nvgm"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/Golgautier/nutanix_vg_manager/releases/download/v0.1.2/nutanix_vg_manager_0.1.2_linux_amd64.tar.gz"
-      sha256 "6351b2092813823d5795e534a18ac9b44b068cb52f57b012a98b62881443910e"
+      url "https://github.com/Golgautier/nutanix_vg_manager/releases/download/v0.1.3/nutanix_vg_manager_0.1.3_linux_amd64.tar.gz"
+      sha256 "219e41c3f9e56aff9c305067459c4aef50eb250b0f6f5df4f9cf6a41ad77168b"
 
       def install
         bin.install "nvgm"
